1a48ba9d453ecf5abf9d2c7c5411183a4b420ded
gsadanala <gsadanala@paypal.com>
2021-03-07 16:42:09 +0530

    Implementing state

--- ageteller/src/components/App.js
+++ ageteller/src/components/App.js
@@ -2,12 +2,39 @@ import React, { Component } from "react";
 import { Form, FormControl, Button } from "react-bootstrap";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newDate: "",
+      birthday: "06/09/1999",
+    };
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  handleDateChange(event) {
+    this.setState({
+      newDate: event.target.value,
+    });
+  }
+
+  handleButtonClick() {
+    console.log(this.state);
+    this.setState({
+      birthday: this.state.newDate,
+    });
+  }
   render() {
     return (
       <div>
         <h1>Input your Birthday!</h1>
         <Form inline>
-          <FormControl type="date"></FormControl> <Button>Submit</Button>
+          <FormControl
+            value={this.state.newDate}
+            type="date"
+            onChange={this.handleDateChange}
+          ></FormControl>
+          <Button onClick={this.handleButtonClick}>Submit</Button>
         </Form>
       </div>
     );
