9deec47c8f71c29768803f4395d8072326462d5a
gsadanala <gsadanala@paypal.com>
2021-03-08 13:03:48 +0530

    Create a Store

--- supersquad/src/index.js
+++ supersquad/src/index.js
@@ -1,5 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/App";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-ReactDom.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer);
+console.log("store.getState()", store.getState());
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
