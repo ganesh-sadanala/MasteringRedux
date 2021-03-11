3d14fdf09895a9a7d1fa342ed714b6aa34c3ebfc
gsadanala <gsadanala@paypal.com>
2021-03-11 10:57:24 +0530

    access the api

--- recipefinder/src/components/SearchRecipes.js
+++ recipefinder/src/components/SearchRecipes.js
@@ -8,18 +8,57 @@ import {
 } from "react-bootstrap";
 
 class SearchRecipes extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      ingredients: "",
+      dish: "",
+    };
+
+    this.handleIngredientsChange = this.handleIngredientsChange.bind(this);
+    this.handleDishChange = this.handleDishChange.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  handleIngredientsChange(event) {
+    this.setState({
+      ingredients: event.target.value,
+    });
+  }
+
+  handleDishChange(event) {
+    this.setState({
+      dish: event.target.value,
+    });
+  }
+
+  search() {
+    let { ingredients, dish } = this.state;
+    const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`;
+  }
   render() {
     return (
       <Form inline>
         <FormGroup>
           <FormLabel>Ingredients</FormLabel>{" "}
-          <FormControl type="text" placeholder="garlic, chicken" />
+          <FormControl
+            type="text"
+            placeholder="garlic, chicken"
+            onChange={this.handleIngredientsChange}
+            value={this.state.ingredients}
+          />
         </FormGroup>{" "}
         <FormGroup>
           <FormLabel>Dish</FormLabel>{" "}
-          <FormControl type="text" placeholder="adobo" />
+          <FormControl
+            type="text"
+            placeholder="adobo"
+            onChange={this.handleDishChange}
+            value={this.state.dish}
+          />
         </FormGroup>{" "}
-        <Button>Submit</Button>
+        <Button onClick={this.search}>Submit</Button>
       </Form>
     );
   }
