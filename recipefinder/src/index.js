4c787e7bfe2170aa86abf4b30123ec70cd477400
gsadanala <gsadanala@paypal.com>
2021-03-11 11:54:36 +0530

    Storing Recipes in store

--- recipefinder/src/index.js
+++ recipefinder/src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
 
-ReactDom.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer);
+store.subscribe(() => console.log("store", store.getState()));
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
