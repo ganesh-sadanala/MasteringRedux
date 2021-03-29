78772a66b0d3ca46b322e9529113cbfa5ed21795
gsadanala <gsadanala@paypal.com>
2021-03-29 16:56:17 +0530

    Crafting a Custom Meme form

--- generator/src/components/App.js
+++ generator/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Form, FormControl, FormGroup, FormLabel } from "react-bootstrap";
 import { connect } from "react-redux";
 import MemeItem from "./MemeItem";
 
@@ -7,12 +8,34 @@ class App extends Component {
     super();
     this.state = {
       memeLimit: 10,
+      text0: "",
+      text1: "",
     };
   }
   render() {
     return (
       <div>
         <h2>Welcome to the Meme Generator</h2>
+        <h4>Write Some Text</h4>
+        <Form inline>
+          <FormGroup>
+            <FormLabel>Top</FormLabel>
+            <FormControl
+              type="text"
+              onChange={(event) => this.setState({ text0: event.target.value })}
+              value={this.state.text0}
+            ></FormControl>{" "}
+          </FormGroup>
+          {"  "}
+          <FormGroup>
+            <FormLabel>Bottom</FormLabel>{" "}
+            <FormControl
+              type="text"
+              onChange={(event) => this.setState({ text1: event.target.value })}
+              value={this.state.text1}
+            ></FormControl>{" "}
+          </FormGroup>
+        </Form>
         {this.props.memes.slice(0, this.state.memeLimit).map((meme, index) => {
           return <MemeItem key={index} meme={meme} />;
         })}
